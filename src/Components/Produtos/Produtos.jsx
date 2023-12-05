3b03a3c525570c73c2f209620ef395c27f27808d
geanemr <geaneramos1995@gmail.com>
2023-12-05 05:55:56 -0300

    chore: products fetch

--- src/Components/Produtos/Produtos.jsx
+++ src/Components/Produtos/Produtos.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import styles from './Produtos.module.css'
+import React from "react";
+import styles from "./Produtos.module.css";
 
 const Produtos = () => {
+  const [produtos, setProdutos] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("https://ranekapi.origamid.dev/json/api/produto")
+      .then((r) => r.json())
+      .then((json) => setProdutos(json));
+  }, []);
+  console.log(produtos);
+  if (produtos === null) {
+    return null;
+  }
   return (
     <div className={styles.produtos}>
-
+      {produtos.map((produto) => (
+        <div key={produto.id}>
+          <h1>{produto.nome}</h1>
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
+        </div>
+      ))}
     </div>
-  )
-}
-export default Produtos
+  );
+};
+export default Produtos;
